d95120407cb4c659a45b646706fd3048f632625b
Matheus <matheusim27@hotmail.com>
2026-07-16 13:53:34 -0300

    initialize project with Fastify, PostgreSQL, and Prisma setup

---
+++ src/lib/prisma.ts
@@ -0,0 +1,13 @@
+import { env } from '@/env'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from 'prisma/generated/prisma/client'
+
+export const schema =
+  new URL(env.DATABASE_URL).searchParams.get('schema') || 'public'
+
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL }, { schema })
+
+export const prisma = new PrismaClient({
+  adapter,
+  log: env.NODE_ENV === 'dev' ? ['query'] : [],
+})
