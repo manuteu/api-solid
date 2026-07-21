e21b6b20790b8abe164c0ca54a9b56e6c6039950
Matheus <matheusim27@hotmail.com>
2026-07-21 09:19:41 -0300

    Add late check-in validation to ValidateCheckInUseCase; introduce LateCheckInValidationError for handling validation errors. Update tests to cover scenarios for check-in validation within 20 minutes of creation.

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,6 +1,8 @@
 import { CheckInsRepository } from '@/repositories/check-ins-repository'
 import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
 import { CheckIn } from 'prisma/generated/prisma/client'
+import { LateCheckInValidationError } from '@/use-cases/errors/late-check-in-validation-error'
+import dayjs from 'dayjs'
 
 interface ValidateCheckInUseCaseRequest {
   checkInId: string
@@ -11,7 +13,7 @@ interface ValidateCheckInUseCaseResponse {
 }
 
 export class ValidateCheckInUseCase {
-  constructor(private checkInsRepository: CheckInsRepository) {}
+  constructor(private checkInsRepository: CheckInsRepository) { }
 
   async execute({
     checkInId,
@@ -23,6 +25,15 @@ export class ValidateCheckInUseCase {
     }
 
     checkIn.validated_at = new Date()
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError()
+    }
 
     await this.checkInsRepository.save(checkIn)
 
