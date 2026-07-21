9e09d277d124d62c013766690bd53127e4d7c91b
Matheus <matheusim27@hotmail.com>
2026-07-21 09:12:22 -0300

    Add findManyNearby method to GymsRepository interface and implement it in InMemoryGymsRepository; create FetchNearbyGymsUseCase with tests for fetching gyms based on user location.

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,4 +1,5 @@
-import { GymsRepository } from '@/repositories/gyms-repository'
+import { FindManyNearbyParams, GymsRepository } from '@/repositories/gyms-repository'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 import { randomUUID } from 'node:crypto'
 import { Gym, Prisma } from 'prisma/generated/prisma/client'
 
@@ -19,6 +20,20 @@ export class InMemoryGymsRepository implements GymsRepository {
     return this.items
       .filter((item) => item.title.includes(query))
       .slice((page - 1) * 20, page * 20)
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        },
+      )
+
+      return distance < 10
+    })
   }
 
   async create(data: Prisma.GymCreateInput) {
