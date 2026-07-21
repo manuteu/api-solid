d0290ea037fd99e21d5ce9384495322ecc4264b2
Matheus <matheusim27@hotmail.com>
2026-07-21 09:03:45 -0300

    Add CreateGym use case and corresponding tests; implement create method in GymsRepository and InMemoryGymsRepository to handle gym creation, along with error classes for distance and check-in limits.

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,5 +1,6 @@
-import { Gym } from 'prisma/generated/prisma/client'
+import { Gym, Prisma } from 'prisma/generated/prisma/client'
 
 export interface GymsRepository {
   findById(id: string): Promise<Gym | null>
+  create(data: Prisma.GymCreateInput): Promise<Gym>
 }
